2f6d5771dc1b712319343db3bc22b80fdc20f838
ImaFruitSnack <4yrmaddict@gmail.com>
2025-06-12 20:09:44 -0500

    Add Files

--- indexx.js
+++ indexx.js
@@ -3,7 +3,6 @@ const bodyParser = require(`body-parser`);
 const path = require('node:path');
 const fs = require(`node:fs`);
 const http = require(`node:http`);
-const dt = require('./myfirstmodule');
 const url = require('url');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = process.env.mongoToken;
@@ -12,3 +11,16 @@ global.mtest = 0;
 global.uservalue = null;
 global.Password = null;
 global.loggedin = null;
+
+const application = express();
+application.use(bodyParser.json())
+application.use(express.static(path.join(__dirname, 'public')));
+application.set('view engine' , 'ejs');
+application.use(express.urlencoded({ extended: true }));
+
+application.get(`/`, async(req, res) => {
+	res.render('pages/index');
+})
+
+let server = http.createServer(application)
+server.listen(8080, `0.0.0.0`)
