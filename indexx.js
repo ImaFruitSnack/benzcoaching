b3707f43300339d067f518a1acceb90aad95a341
ImaFruitSnack <4yrmaddict@gmail.com>
2025-06-16 18:29:28 -0500

    Add files

--- indexx.js
+++ indexx.js
@@ -32,6 +32,7 @@ application.get(`/`, async(req, res) => {
 	res.render('pages/index');
 	let data = await fs.readFileSync('./views/pages/index.ejs');
 	encryptt('test')
+	console.log(await hash);
 	res.write(data);
 })
 
